3c6e2c33dd188f78be84f52eb4dd2b9d24719a36
Ken Brewer <kenibrewer@users.noreply.github.com>
2024-10-22 17:42:16 -0700

    🎉 init: kenibrewer/

---
+++ .devcontainer/.project_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $PWD == /tmp* ]]; then
+    echo "We are /tmp/. This is copier update. Exiting"
+    exit 0
+fi
+
+if [ -d .git/ ]; then
+  echo "This project is already initialized. Exiting"
+  exit 0
+fi
+
+git init --initial-branch=main
+git add .
+git commit -a -m '🎉 init: kenibrewer/'
+
+# here more initilization stuff
